7c9023de251413fe8a126d58c8a049b8a24e253e
discomfitor <mike@zentific.com>
2011-07-21 19:59:27 -0400

    significantly improve build.sh

--- build.sh
+++ build.sh
@@ -5,42 +5,23 @@ CF="-D_GNU_SOURCE=1 -O0 -pipe -Wall -Wextra -g"
 DEPS=($(pkg-config --print-requires-private ecore-con))
 echo "DEPENDENCIES: ${DEPS[@]}"
 CFLAGS="$(pkg-config --static --cflags ${DEPS[@]} ecore-con)"
-#CFLAGS+=" -I/usr/include/sasl"
 echo "DEPENDENCY CFLAGS: $CFLAGS"
 
 LIBS="$(pkg-config --static --libs ${DEPS[@]} ecore-con)"
-if (echo "$LIBS" | grep gnutls &> /dev/null) ; then
-	LIBS+=" $(pkg-config --static --libs gnutls)"
-fi
-#LIBS+=" -lsasl2"
 echo "DEPENDENCY LIBS: $LIBS"
 echo
 
-echo "g++ -c pugixml.cpp -o pugixml.o || exit 1"
-g++ -c pugixml.cpp -o pugixml.o || exit 1
+for x in *.c ; do
+	[[ -f "${x/.c/.o}" && "$x" -ot "${x/.c/.o}" ]] && continue
+	echo "gcc -c $x -o ${x/.c/.o} $CFLAGS $CF || exit 1"
+	gcc -c $x -o "${x/.c/.o}" $CFLAGS $CF || exit 1
+done
 
-echo "gcc -c cdecode.c -o cdecode.o || exit 1"
-gcc -c cdecode.c -o cdecode.o || exit 1
-echo "gcc -c cencode.c -o cencode.o || exit 1"
-gcc -c cencode.c -o cencode.o || exit 1
-echo "gcc -c shotgun_utils.c -o shotgun_utils.o || exit 1"
-gcc -c shotgun_utils.c -o shotgun_utils.o || exit 1
-
-
-echo "g++ -c xml.cpp -o xml.o $CFLAGS $CF || exit 1"
-g++ -c xml.cpp -o xml.o $CFLAGS $CF || exit 1
-echo "gcc -c getpass_x.c -o getpass_x.o $CF || exit 1"
-gcc -c getpass_x.c -o getpass_x.o $CF || exit 1
-echo "gcc -c shotgun.c -o shotgun.o $CFLAGS $CF || exit 1"
-gcc -c shotgun.c -o shotgun.o $CFLAGS $CF || exit 1
-echo "gcc -c login.c -o login.o $CFLAGS $CF || exit 1"
-gcc -c login.c -o login.o $CFLAGS $CF || exit 1
-echo "gcc -c messaging.c -o messaging.o $CFLAGS $CF || exit 1"
-gcc -c messaging.c -o messaging.o $CFLAGS $CF || exit 1
-echo "gcc -c iq.c -o iq.o $CFLAGS $CF || exit 1"
-gcc -c iq.c -o iq.o $CFLAGS $CF || exit 1
-echo "gcc -c presence.c -o presence.o $CFLAGS $CF || exit 1"
-gcc -c presence.c -o presence.o $CFLAGS $CF || exit 1
+for x in *.cpp ; do
+	[[ -f "${x/.cpp/.o}" && "$x" -ot "${x/.cpp/.o}" ]] && continue
+	echo "gcc -c $x -o ${x/.cpp/.o} $CFLAGS $CF || exit 1"
+	gcc -c $x -o "${x/.cpp/.o}" $CFLAGS $CF || exit 1
+done
 
 echo "g++ *.o -o shotgun -L/usr/lib -lc $LIBS" #pugixml.a
 g++ *.o -o shotgun -L/usr/lib -lc $LIBS #pugixml.a
