e456580290078409adf8bcf1ab84adeb749ab013
discomfitor <mike@zentific.com>
2011-07-22 16:43:53 -0400

    always rebuild when headers change

--- build.sh
+++ build.sh
@@ -12,9 +12,19 @@ LIBS="$(pkg-config --static --libs ${DEPS[@]} ecore-con elementary)"
 #echo
 
 link=0
+compile=0
+
+if [[ -f ./shotgun ]] ; then
+	for x in *.h ui/*.h ; do
+		if [[ "$x" -nt ./shotgun ]] ; then
+			compile=1
+			break;
+		fi
+	done
+fi
 
 for x in *.c ui/*.c  ; do
-	[[ -f "${x/.c/.o}" && "$x" -ot "${x/.c/.o}" ]] && continue
+	[[ $compile == 0 && -f "${x/.c/.o}" && "$x" -ot "${x/.c/.o}" ]] && continue
 #	echo "gcc -c $x -o ${x/.c/.o} $CFLAGS $CF || exit 1"
 	echo "gcc -c $x -o ${x/.c/.o} || exit 1"
 	gcc -c $x -o "${x/.c/.o}" $CFLAGS $CF || exit 1
@@ -22,14 +32,14 @@ for x in *.c ui/*.c  ; do
 done
 
 for x in *.cpp ; do
-	[[ -f "${x/.cpp/.o}" && "$x" -ot "${x/.cpp/.o}" ]] && continue
+	[[ $compile == 0 && -f "${x/.cpp/.o}" && "$x" -ot "${x/.cpp/.o}" ]] && continue
 #	echo "gcc -c $x -o ${x/.cpp/.o} $CFLAGS $CF || exit 1"
 	echo "gcc -c $x -o ${x/.cpp/.o} || exit 1"
 	gcc -c $x -o "${x/.cpp/.o}" $CFLAGS $CF || exit 1
 	link=1
 done
 
-[[ link == 0 ]] && exit 1
+[[ $link == 0 ]] && exit 1
 
 #echo "g++ *.o -o shotgun -L/usr/lib -lc $LIBS" #pugixml.a
 echo "g++ *.o -o shotgun" #pugixml.a
