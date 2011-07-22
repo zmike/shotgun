49b151b220a3ddef3937a1e240a831162c9ec987
discomfitor <mike@zentific.com>
2011-07-21 21:16:53 -0400

    actually make contact list

--- shotgun.c
+++ shotgun.c
@@ -5,6 +5,7 @@
 #include <ctype.h>
 
 #include "xml.h"
+#include "ui/ui.h"
 
 int shotgun_log_dom = -1;
 
@@ -231,6 +232,7 @@ main(int argc, char *argv[])
    auth.resource = eina_stringshare_add("SHOTGUN!");
 
    shotgun_gchat_connect(&auth);
+   contact_list_new(argc, argv);
    ecore_main_loop_begin();
 
    return 0;
