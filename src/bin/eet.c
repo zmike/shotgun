cfe6dc0e1e146283fb6c94a9d29e7fee1fc866a5
discomfitor <mike@zentific.com>
2011-07-30 01:26:32 -0400

    use eet_list instead of stupid eet_dictionaries

--- src/bin/eet.c
+++ src/bin/eet.c
@@ -62,50 +62,55 @@ void
 ui_eet_image_add(const char *url, Eina_Binbuf *buf)
 {
    const char *sha1;
-   Eet_Dictionary *ed;
+   int lsize;
+   char **list;
 
    if (!images) return;
 
    sha1 = sha1_buffer(eina_binbuf_string_get(buf), eina_binbuf_length_get(buf));
    INF("Image: %s - %s", url, sha1);
-   ed = eet_dictionary_get(images);
-   if (eet_dictionary_string_check(ed, url))
+   list = eet_list(images, url, &lsize);
+   if (lsize)
      {
         eina_stringshare_del(sha1);
+        free(list);
         return; /* should never happen */
      }
-   if (eet_dictionary_string_check(ed, sha1))
+   list = eet_list(images, sha1, &lsize);
+   if (lsize)
      {
         eet_alias(images, sha1, url, 1);
+        eet_sync(images);
         INF("Added new alias for image %s", sha1);
         eina_stringshare_del(sha1);
+        free(list);
         return;
      }
    eet_write(images, sha1, eina_binbuf_string_get(buf), eina_binbuf_length_get(buf), 1);
    eet_alias(images, sha1, url, 1);
+   eet_sync(images);
    INF("Added new image with length %zu: %s", eina_binbuf_length_get(buf), sha1);
 }
 
 Eina_Binbuf *
 ui_eet_image_get(const char *url)
 {
-   const char *sha1;
    size_t size;
    unsigned char *img;
    Eina_Binbuf *buf = NULL;
+   char **list;
+   int lsize;
+
    if (!images) return NULL;
-   Eet_Dictionary *ed;
 
-   ed = eet_dictionary_get(images);
-   if (!eet_dictionary_string_check(ed, url)) return NULL;
-   sha1 = eet_alias_get(images, url);
-   EINA_SAFETY_ON_NULL_RETURN_VAL(sha1, NULL);
+   list = eet_list(images, url, &lsize);
+   if (!lsize) return NULL;
+   free(list);
 
-   img = eet_read(images, sha1, (int*)&size);
+   img = eet_read(images, url, (int*)&size);
    buf = eina_binbuf_new(); /* FIXME: eina_binbuf_managed_new */
    eina_binbuf_append_length(buf, img, size);
 
    free(img);
-   eina_stringshare_del(sha1);
    return buf;
 }
