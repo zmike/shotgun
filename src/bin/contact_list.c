05dfd72f58e3a89414f7b248dffc942fa1739c0f
discomfitor <mike@zentific.com>
2011-08-10 20:54:33 -0400

    use the right pointers for stuff

--- src/bin/contact_list.c
+++ src/bin/contact_list.c
@@ -306,12 +306,12 @@ _contact_list_item_tooltip_cb(Contact *c, Evas_Object *obj __UNUSED__, Evas_Obje
                                   "<b><subtitle><u>%s (%i)%c</u></subtitle></b><ps>"
                                   "%s%s",
                                   c->base->jid,
-                                  c->cur->jid + strlen(c->base->jid) + 1, c->cur->priority, c->description ? ':' : 0,
+                                  c->cur->jid + strlen(c->base->jid) + 1, c->priority, c->description ? ':' : 0,
                                   c->description ?: "", c->description ? "<ps>" : "");
    EINA_LIST_FOREACH(c->plist, l, p)
      eina_strbuf_append_printf(buf, "<b>%s (%i)%c</b><ps>"
                                     "%s%s",
-                                    p->jid + strlen(c->base->jid) + 1, c->cur->priority, c->description ? ':' : 0,
+                                    p->jid + strlen(c->base->jid) + 1, c->priority, c->description ? ':' : 0,
                                     p->description ?: "", p->description ? "<ps>" : "");
    text = eina_stringshare_add(eina_strbuf_string_get(buf));
    eina_strbuf_free(buf);
