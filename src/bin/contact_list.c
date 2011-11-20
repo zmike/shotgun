4e512f81ad2f3301b1f666472db3b95bf9a90dd6
discomfitor <mike@zentific.com>
2011-11-20 00:36:55 -0500

    sort of fix sizing issue with naviframe

--- src/bin/contact_list.c
+++ src/bin/contact_list.c
@@ -153,7 +153,7 @@ _contact_list_add_cb(Contact_List *cl, Evas_Object *obj __UNUSED__, Elm_Toolbar_
    if (cl->pager) return;
    cl->pager = p = elm_naviframe_add(cl->win);
    WEIGHT(p, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-   ALIGN(p, EVAS_HINT_FILL, 0);
+   ALIGN(p, EVAS_HINT_FILL, EVAS_HINT_FILL);
    elm_box_pack_after(cl->box, p, cl->list);
    elm_object_style_set(p, "slide");
 
