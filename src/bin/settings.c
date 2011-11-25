cb22decf67b94e692943b675fd04b948d183f60d
discomfitor <mike@zentific.com>
2011-11-25 16:41:14 -0500

    put settings into scroller

--- src/bin/settings.c
+++ src/bin/settings.c
@@ -3,7 +3,7 @@
 void
 settings_new(Contact_List *cl)
 {
-   Evas_Object *ic, *back, *box, *ck, *fr, *frbox;
+   Evas_Object *scr, *ic, *back, *box, *ck, *fr, *frbox;
 
    cl->settings_box = box = elm_box_add(cl->win);
    EXPAND(box);
@@ -21,6 +21,22 @@ settings_new(Contact_List *cl)
    elm_box_pack_end(box, back);
    evas_object_smart_callback_add(back, "clicked", (Evas_Smart_Cb)settings_toggle, cl);
    evas_object_show(back);
+   elm_flip_content_back_set(cl->flip, box);
+
+   scr = elm_scroller_add(cl->win);
+   EXPAND(scr);
+   FILL(scr);
+   elm_box_pack_end(box, scr);
+
+   box = elm_box_add(cl->win);
+   EXPAND(box);
+   FILL(box);
+   evas_object_show(box);
+
+   elm_object_content_set(scr, box);
+   elm_scroller_bounce_set(scr, EINA_FALSE, EINA_FALSE);
+   elm_scroller_policy_set(scr, ELM_SCROLLER_POLICY_OFF, ELM_SCROLLER_POLICY_AUTO);
+   evas_object_show(scr);
 
    fr = elm_frame_add(cl->win);
    EXPAND(fr);
@@ -102,8 +118,6 @@ settings_new(Contact_List *cl)
    elm_check_state_pointer_set(ck, &cl->settings.enable_chat_promote);
    elm_box_pack_end(frbox, ck);
    evas_object_show(ck);
-
-   elm_flip_content_back_set(cl->flip, box);
 }
 
 void
