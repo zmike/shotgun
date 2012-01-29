f6c77cde3684f562634b7b38a5ecd3bfb9ea099a
discomfitor <mike@zentific.com>
2012-01-29 14:37:45 -0500

    elm_genlist_item_del -> elm_object_item_del

--- src/bin/contact_list.c
+++ src/bin/contact_list.c
@@ -1098,7 +1098,7 @@ contact_list_init(UI_WIN *ui, Shotgun_Auth *auth)
    cl->list_selected_item_get[1] = (Ecore_Data_Cb)elm_gengrid_selected_item_get;
    cl->list_item_parent_get[0] = (Ecore_Data_Cb)elm_genlist_item_genlist_get;
    cl->list_item_parent_get[1] = (Ecore_Data_Cb)elm_genlist_item_genlist_get;
-   cl->list_item_del[0] = (Ecore_Cb)elm_genlist_item_del;
+   cl->list_item_del[0] = (Ecore_Cb)elm_object_item_del;
    cl->list_item_del[1] = (Ecore_Cb)elm_gengrid_item_del;
    cl->list_item_update[0] = (Ecore_Cb)elm_genlist_item_update;
    cl->list_item_update[1] = (Ecore_Cb)elm_gengrid_item_update;
